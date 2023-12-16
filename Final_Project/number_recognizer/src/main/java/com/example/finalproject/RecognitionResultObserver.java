4c160bfb715d1cd6d135622946913a54909f1da5
Weiwei Zhuo <115064974+zhuoww@users.noreply.github.com>
2023-12-16 11:37:43 -0800

    Delete Final_Project directory

--- Final_Project/number_recognizer/src/main/java/com/example/finalproject/RecognitionResultObserver.java
+++
@@ -1,6 +0,0 @@
-package Final_Project.number_recognizer.src.main.java.com.example.finalproject;
-
-public interface RecognitionResultObserver {
-    // Method to be called when a recognition result is updated
-    void updateRecognitionResult(int result);
-}
